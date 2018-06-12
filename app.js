9883ae33daf1848c2b95b296e257b5852f45cc74
Kara Cavanaugh <karacav@msn.com>
2018-06-11 23:06:53 -0400

    step 1

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-var cat = "Cool Beans"; console.log(cat);
+const triggers = document.querySelectorAll('.cool > li');
+const background = document.querySelector('.dropdownBackground');
+const nav = document.querySelector('.top');
+function handleEnter() {
+    this.classList.add('trigger-enter');
+    setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150);
+    background.classList.add('open');
+    const dropdown = this.querySelector('.dropdown');
+    const dropdownCoords = dropdown.getBoundingClientRect();
+    const navCoords = nav.getBoundingClientRect();
